cce0532cef50cf63eb8053ab7279a16339807190
AvidIndoorsman2022 <avidIndoorsman2021@gmail.com>
2022-02-26 12:31:54 -0500

    Incremented version, added gh-pages.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  base: "/safeguard-send-dashboard/",
+  // Commented out result:
+  //   "npm run build"
+  //   "npm run deploy"
+  //   result: updated locally on /dist
+  //   result: update remotely on /gh-pages
+  //   result: no update on git
+
+  // Uncommented out result:
+  //   "npm run build"
+  //   "npm run deploy"
+  //   result: updated locally on /dist
+  //   result: update remotely on /gh-pages
+  //   result: index.html update on git
+});
